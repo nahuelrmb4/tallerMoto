77ad7c2b8db0935b34c06c3a83a478c0ab2fb049
Nahue <nahuelrmb4@gmail.com>
2023-10-04 19:53:45 -0300

    update class 4-10-23

--- motores/models.py
+++ motores/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 
+class Marca(models.Model):
+    nombre =models.CharField(max_length=30)
+
 class Motor(models.Model):
     codigo= models.CharField(max_length=20)
+    marca=models.ManyToManyField(Marca)
+
     
 def __str__(self):
     return self.codigo
